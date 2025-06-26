d1caccb40694a37424df6f8175387646c8668c3b
Sabeer9999 <sabeersunny783@gmail.com>
2025-06-26 11:22:03 +0530

    sa

---
+++ python_jun24.py
@@ -0,0 +1,78 @@
+#out put print
+print("Sabeer")
+print ("hello")
+
+print (1+6)
+print (2+6)
+print (2-9)
+print (9*8)
+#membership operators
+a=2
+b=a
+print(a is b)
+
+a=1
+print(type(a))
+
+#data type
+a=1+7j
+b=2+9j
+print(a+b)
+
+#boolean data type
+
+a= True
+print(type(a))
+
+print("hello","sabeer")
+print ("Hi","sunny!")
+a=10
+b=20
+print(a+b)
+
+a=30
+b=40
+print(a-b)
+
+x=40
+y=2
+print(x*y)
+
+x=20
+y=50
+print(x/y)
+
+
+
+a=25
+b=50
+print (a<=b)
+
+a=22
+b=30
+print(a>=b)
+#  in , not in
+emails=["Sabeer@gmail.com","sunny@gmail.com","Raja@gmail.com"]
+email="Raja@gmail.com"
+print(email in emails)
+
+#  in , not in
+emails=["joel@gmail.com","srinu@gmail.com","praveen@gmail.com"]
+email="praveen@gmail.com"
+print(email not in emails)
+
+#is , is not
+a=1
+b=a
+print(a is b)
+a=7
+b=a
+print (a is not b)
+
+# And , or , not
+
+a=20
+print(a>1 or a<99)
+
+a=30
+print (not(a>10 and a<20))
